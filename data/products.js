22fe07f7946e0a9c8009cafd0db3d48a641d1b99
handryhua <58896496+handryhua@users.noreply.github.com>
2026-03-13 22:38:59 +0700

    Create data/products.js

---
+++ data/products.js
@@ -0,0 +1,75 @@
+export const products = [
+
+{ sku:"6865", name:"ADAPTOR (BPM)", price:98011 },
+
+{ sku:"B438", name:"BLOOD PRESSURE MONITOR HEM-6232T", price:1081336 },
+
+{ sku:"D166", name:"BLOOD PRESSURE MONT HBP1120", price:1554000 },
+
+{ sku:"D165", name:"BLOOD PRESSURE MONT HBP1320", price:3108000 },
+
+{ sku:"E040", name:"BLOOD PRESSURE MONT HBP9031C", price:27454000 },
+
+{ sku:"8085", name:"BLOOD PRESSURE MONT HEM 7120", price:408000 },
+
+{ sku:"D238", name:"BLOOD PRESSURE MONT HEM 7143T1", price:491600 },
+
+{ sku:"E250", name:"BLOOD PRESSURE MONT HEM 7156-AP", price:577280 },
+
+{ sku:"D526", name:"BLOOD PRESSURE MONT HEM 7156T", price:619500 },
+
+{ sku:"D239", name:"BLOOD PRESSURE MONT HEM 7156T-A", price:783000 },
+
+{ sku:"D163", name:"BLOOD PRESSURE MONT HEM 907", price:7770000 },
+
+{ sku:"E658", name:"BLOOD PRESSURE MONT HEM-7142T2", price:412000 },
+
+{ sku:"C332", name:"BLOOD PRESSURE MONT HEM-7361T", price:1229500 },
+
+{ sku:"F140", name:"BLOOD PRESSURE MONT HEM-7383T1", price:1230000 },
+
+{ sku:"E670", name:"BLOOD PRESSURE MONT JPN-750", price:702512 },
+
+{ sku:"E457", name:"BLOOD PRESSURE MONT+ECG HEM-7530T", price:2492440 },
+
+{ sku:"6830", name:"BODY FAT MONITOR HBF-375", price:1641320 },
+
+{ sku:"6863", name:"CUFF - L HEM-RML31-BAP", price:145449 },
+
+{ sku:"6862", name:"CUFF - M HEM-CR24-BAP", price:109238 },
+
+{ sku:"6861", name:"CUFF - S HEM-CS24-BAP", price:97100 },
+
+{ sku:"8375", name:"EAR THERMOMETER TH-8395", price:452153 },
+
+{ sku:"B292", name:"NEBULIZER NEC 101", price:429250 },
+
+{ sku:"4682", name:"NEBULIZER NE-C28", price:619500 },
+
+{ sku:"D252", name:"NEBULIZER NE-C28 NEW", price:619500 },
+
+{ sku:"7727", name:"NEBULIZER NE-C801", price:468500 },
+
+{ sku:"D162", name:"NEBULIZER NE-C900", price:1243200 },
+
+{ sku:"B304", name:"NEBULIZER NEU100", price:2079818 },
+
+{ sku:"D161", name:"NEBULIZER NE-U780", price:8288000 },
+
+{ sku:"6880", name:"PROBE COVER TH839S", price:119576 },
+
+{ sku:"B735", name:"PULSE MASSAGER HV-F013", price:445493 },
+
+{ sku:"E770", name:"STAND KIT HEM-907", price:2690600 },
+
+{ sku:"E245", name:"THERMOMETER MC-246 AP4", price:56880 },
+
+{ sku:"E297", name:"THERMOMETER MC-341 AP4", price:59940 },
+
+{ sku:"E089", name:"THERMOMETER MC-343F", price:78220 },
+
+{ sku:"F139", name:"THERMOMETER MC-F300 AP", price:452000 },
+
+{ sku:"8504", name:"WEIGHT SCALE HN 289 (HITAM)", price:227739 }
+
+];
